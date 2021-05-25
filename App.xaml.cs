916a517d84b3c32331414e972486a97801957cdc
Joygen Zhang <zhangfl0220@163.com>
2021-05-25 21:11:37 +0800

    First Version

---
+++ App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Ioc;
+using Prism.Unity;
+using TIAOpenness.ViewModels;
+
+namespace TIAOpenness
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+public partial class App : PrismApplication
+{
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        //创建服务
+        // containerRegistry.Register<Services.ICustomerStore, Services.DbCustomerStore>();
+        containerRegistry.RegisterForNavigation<MainWindow, MainWindow1ViewModel>();
+
+        }
+
+    protected override Window CreateShell()
+    {
+        var w = Container.Resolve<MainWindow>();
+        return w;
+    }
+}
+}
---
+++ ViewModels/MainWindow1ViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Prism.Commands;
+using Prism.Mvvm;
+
+namespace TIAOpenness.ViewModels
+{
+    public class MainWindow1ViewModel : BindableBase
+
+    {
+        private string _hello;
+
+        public string Hello
+        {
+            get => _hello;
+            set => SetProperty(ref _hello, value);
+        }
+
+        private DelegateCommand _nameClick;
+
+        public DelegateCommand NameClick =>
+            _nameClick ?? (_nameClick = new DelegateCommand(ExecuteNameClick));
+
+        void ExecuteNameClick()
+        {
+        }
+
+        private DelegateCommand _clickCommand;
+
+        public DelegateCommand ClickCommand =>
+            _clickCommand ?? (_clickCommand = new DelegateCommand(ExecuteClickCommand));
+
+        void ExecuteClickCommand()
+        {
+            Debug.Print("Hello");
+        }
+    }
+}
